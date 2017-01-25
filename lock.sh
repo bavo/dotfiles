936df205914e71d6c21f4f9ac11b3e7caf58f7ad
Bavo Mees <Bavo.Mees@hpcds.com>
2017-01-25 12:13:24 +0100

    lock and suspend functions for i3

---
+++ lock.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+ 
+scrot /tmp/screen.png
+
+HUE=(-level 0%,100%,0.6)
+EFFECT=(-filter Gaussian -resize 20% -define filter:sigma=1.5 -resize 500.5%)
+
+convert /tmp/screen.png "${HUE[@]}" "${EFFECT[@]}" /tmp/screen.png
+ 
+if [ -f $HOME/dotfiles/screen-lock.png ]
+then
+    # placement x/y
+    PX=0
+    PY=0
+    # lockscreen image info
+    R=$(file ~/dotfiles/screen-lock.png | grep -o '[0-9]* x [0-9]*')
+    RX=$(echo $R | cut -d' ' -f 1)
+    RY=$(echo $R | cut -d' ' -f 3)
+ 
+    SR=$(xrandr --query | grep ' connected' | sed 's/primary //' | cut -f3 -d' ')
+    for RES in $SR
+    do
+        # monitor position/offset
+        SRX=$(echo $RES | cut -d'x' -f 1)                   # x pos
+        SRY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 1)  # y pos
+        SROX=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 2) # x offset
+        SROY=$(echo $RES | cut -d'x' -f 2 | cut -d'+' -f 3) # y offset
+        PX=$(($SROX + $SRX/2 - $RX/2))
+        PY=$(($SROY + $SRY/2 - $RY/2))
+ 
+        convert /tmp/screen.png $HOME/dotfiles/screen-lock.png -geometry +$PX+$PY -composite -matte  /tmp/screen.png
+        echo "done"
+    done
+fi
+# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+# i3lock  -I 10 -d -e -u -n -i /tmp/screen.png
+i3lock -e -u -n -i /tmp/screen.png
+rm /tmp/screen.png
